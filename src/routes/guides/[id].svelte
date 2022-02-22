1c0fa6d07fb37607c24e02fcbe70c0478f41b713
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2022-02-22 13:32:25 +0700

    add back button

--- src/routes/guides/[id].svelte
+++ src/routes/guides/[id].svelte
@@ -28,6 +28,7 @@
   <h2>{guide.title}</h2>
   <p>{guide.body}</p>
   <pre>Created by : {guide.userId}</pre>
+  <a href="/guides"><button>Back</button></a>
 </div>
 
 <style>
@@ -40,5 +41,20 @@
   pre {
     color: #c4c4c4;
     font-size: 10px;
+  }
+
+  button {
+    padding: 5px 20px;
+    background-color: #422d5d;
+    color: #decff1;
+    border: 1px solid #decff1;
+    font-size: 9px;
+    border-radius: 8px;
+  }
+
+  button:hover {
+    background-color: #decff1;
+    color: #422d5d;
+    border: 1px solid transparent;
   }
 </style>
